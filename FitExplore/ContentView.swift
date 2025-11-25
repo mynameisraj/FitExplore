b9c711e476d50443c9b9ad0a62aaffa1306992f3
Raj Ramamurthy <raj.r@me.com>
2025-11-24 17:46:45 -0700

    Initial commit

---
+++ FitExplore/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FitExplore
+//
+//  Created by Raj Ramamurthy on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: FitExploreDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+#Preview {
+    ContentView(document: .constant(FitExploreDocument()))
+}
---
+++ FitExplore/FitExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitExploreApp.swift
+//  FitExplore
+//
+//  Created by Raj Ramamurthy on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FitExploreApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: FitExploreDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
